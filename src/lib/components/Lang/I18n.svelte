1ca6a37cb50763f7797e3109aaedf28a3366e6b3
herbinator222 <herbinator@posteo.de>
2023-09-02 07:44:44 +0200

    Added german translation

--- src/lib/components/Lang/I18n.svelte
+++ src/lib/components/Lang/I18n.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
 	import { settings } from '$lib/stores/settings.store';
 	import { addMessages, init, locale } from 'svelte-i18n';
+
+	import de from '../../lang/de.json';
 	import en from '../../lang/en.json';
 	import es from '../../lang/es.json';
 	import fr from '../../lang/fr.json';
 	import it from '../../lang/it.json';
 
+	addMessages('de', de);
 	addMessages('en', en);
 	addMessages('es', es);
 	addMessages('fr', fr);
