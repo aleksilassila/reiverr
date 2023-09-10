1221c543ddead9afc96659bd6d194b47f09b46ca
Francisco Zorat <franciscozorat@gmail.com>
2023-09-10 09:35:21 -0300

    tooltip mutebutton

--- src/lib/components/VideoPlayer/components/MuteButton.svelte
+++ src/lib/components/VideoPlayer/components/MuteButton.svelte
@@ -15,8 +15,7 @@
     <VolumeHighIcon class="hidden group-data-[volume=high]:block" type="volume-high"/>
 	<!-- tooltip -->
 	<media-tooltip
-		class="ease pointer-events-none absolute bottom-full left-1/2 mb-4 -translate-x-1/2 translate-y-2.5 scale-75 whitespace-nowrap rounded-sm bg-black py-1 px-2 font-sans text-xs text-white opacity-0 transition-all duration-200 group-data-[hocus]:-translate-x-1/2 group-data-[hocus]:scale-100 group-data-[hocus]:opacity-100"
-		style="transform-origin: 50% 100%"
+		class="rounded-md bg-zinc-800/90 px-2.5 py-1.5 font-bold"
 	>
 		<span class="not-muted:inline hidden">Mute</span>
 		<span class="muted:inline hidden">Unmute</span>
