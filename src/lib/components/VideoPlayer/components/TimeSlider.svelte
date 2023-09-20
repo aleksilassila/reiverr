5a2e1f764564ade325396b27990507e26d3d93a5
Francisco Zorat <franciscozorat@gmail.com>
2023-09-19 21:07:56 -0300

    slider thumbs

--- src/lib/components/VideoPlayer/components/TimeSlider.svelte
+++ src/lib/components/VideoPlayer/components/TimeSlider.svelte
@@ -6,7 +6,7 @@
 		track-fill-class="absolute top-1/2 left-0 z-20 h-0.5 w-[var(--slider-fill-percent)] -translate-y-1/2 bg-amber-400/80 will-change-[width]"
 		track-progress-class="absolute top-1/2 left-0 z-10 h-0.5 w-[var(--media-buffered-percent)] -translate-y-1/2 bg-zinc-400/60 will-change-[width]"
 		thumb-container-class="absolute top-0 left-[var(--slider-fill-percent)] z-20 h-full w-3 -translate-x-1/2 group-data-[dragging]:left-[var(--slider-pointer-percent)] items-center"
-		thumb-class="absolute top-1/2 left-0 h-2.5 w-3 -translate-y-1/2 rounded-full bg-amber-400 opacity-0 transition-opacity duration-150 ease-in group-data-[interactive]:opacity-100"
+		thumb-class="absolute top-1/2 left-0 h-2.5 w-2.5 -translate-y-1/2 rounded-full bg-amber-400 opacity-0 transition-opacity duration-150 ease-in group-data-[interactive]:opacity-100"
 		chapters-class="relative flex items-center w-full h-full"
 		chapter-container-class="flex items-center w-[var(--width)] h-full mr-0.5 last:mr-0"
 		chapter-class="relative flex items-center w-full h-1"
--- src/lib/components/VideoPlayer/components/VolumeSlider.svelte
+++ src/lib/components/VideoPlayer/components/VolumeSlider.svelte
@@ -1,12 +1,12 @@
 <div
-	class="opacity-0 transition-opacity duration-200 ease-out group-hover:opacity-100 group-hover:ease-in"
+	class="opacity-0 transition-opacity duration-200 ease-out group-hover:opacity-100 group-hover:ease-in font-fira"
 >
 	<media-volume-slider
 		class="group my-2.5 flex h-10 w-20 items-center"
-		track-class="absolute bottom-0 left-0 z-0 h-1 w-20 bg-amber-400/20 outline-none group-data-[focus]:ring-4 group-data-[focus]:ring-amber-600"
-		track-fill-class="absolute bottom-0 z-20 w-[var(--slider-fill-percent)] h-1 bg-zinc-200 will-change-[width]"
+		track-class="absolute bottom-0 left-0 z-0 h-0.5 w-20 bg-amber-400/20 outline-none group-data-[focus]:ring-4 group-data-[focus]:ring-amber-600"
+		track-fill-class="absolute bottom-0 z-20 w-[var(--slider-fill-percent)] h-0.5 bg-zinc-200 will-change-[width]"
 		thumb-container-class="absolute z-20 w-full h-10 group-data-[dragging]:bottom-[var(--slider-pointer-percent)]"
-		thumb-class="absolute bottom-0 left-1/2 h-2.5 w-3 rounded-full bg-yellow-400 opacity-0 transition-opacity duration-150 ease-in group-data-[interactive]:opacity-100"
+		thumb-class="absolute bottom-0 left-1/2 h-2.5 w-2.5 rounded-full bg-yellow-400 opacity-0 transition-opacity duration-150 ease-in group-data-[interactive]:opacity-100"
 	>
 		<div
 			class="absolute flex items-center justify-center rounded-sm px-1 py-px text-white/80 opacity-0 transition-opacity duration-200 ease-out group-data-[interactive]:opacity-100 group-data-[interactive]:ease-in"
@@ -15,7 +15,7 @@
 			<media-slider-value
 				type="pointer"
 				format="percent"
-				class="bg-zinc-800 text-zinc-300 rounded-md font-bold"
+				class="bg-zinc-800 text-zinc-300 rounded-md font-medium"
 			/>
 		</div>
 	</media-volume-slider>
