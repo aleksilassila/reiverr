5b4511eac735919b6a0ec456528080aae0595117
Aleksi Lassila <aleksi.emil.lassila@gmail.com>
2024-06-09 23:00:10 +0300

    fix: Fix infinite scroll not fetching the second page on big screens without scroll

--- src/lib/components/GridPage/InfiniteScroll.svelte
+++ src/lib/components/GridPage/InfiniteScroll.svelte
@@ -1,82 +1,31 @@
-<!-- Source: https://github.com/andrelmlins/svelte-infinite-scroll/blob/main/src/lib/InfiniteScroll.svelte -->
-
 <script lang="ts">
 	import { onMount, onDestroy, createEventDispatcher } from 'svelte';
 
-	export let threshold: number | undefined = 0;
-	export let horizontal: boolean | undefined = false;
-	export let elementScroll: HTMLElement | null = null;
+	const dispatch = createEventDispatcher<{ loadMore: null }>();
+
 	export let hasMore: boolean | undefined = true;
-	export let reverse: boolean | undefined = false;
-	export let window: boolean | undefined = false;
 
-	const dispatch = createEventDispatcher<{ loadMore: never }>();
+	let observer: IntersectionObserver;
+	let loadMoreElement: HTMLElement;
 
-	let isLoadMore: boolean = false;
-	let component: HTMLElement;
-	let beforeScrollHeight: number;
-	let beforeScrollTop: number;
-	let element: any | null;
-
-	$: if (element) {
-		if (reverse) {
-			element.scrollTop = element.scrollHeight;
-		}
-		element.addEventListener('scroll', onScroll);
-		element.addEventListener('resize', onScroll);
-	}
-
-	$: if (isLoadMore && reverse) {
-		element.scrollTop = element.scrollHeight - beforeScrollHeight + beforeScrollTop;
-	}
-
-	const onScroll = (e: Event) => {
-		if (!hasMore) return;
-
-		const target = e.target as HTMLElement;
-		const offset = calcOffset(target, reverse!, horizontal!);
-
-		if (offset <= threshold!) {
-			if (!isLoadMore && hasMore) {
+	const loadMore = (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting && hasMore) {
 				dispatch('loadMore');
-				beforeScrollHeight = target.scrollHeight;
-				beforeScrollTop = target.scrollTop;
 			}
-			isLoadMore = true;
-		} else {
-			isLoadMore = false;
-		}
-	};
-
-	const calcOffset = (target: any, reverse: boolean, horizontal: boolean) => {
-		const element: HTMLElement = target.documentElement ? target.documentElement : target;
-
-		if (reverse) {
-			return horizontal ? element.scrollLeft : element.scrollTop;
-		}
-		return horizontal
-			? element.scrollWidth - element.clientWidth - element.scrollLeft
-			: element.scrollHeight - element.clientHeight - element.scrollTop;
+		});
 	};
 
 	onMount(() => {
-		if (window) {
-			element = document;
-		} else if (elementScroll) {
-			element = elementScroll;
-		} else {
-			element = component.parentNode;
-		}
+		observer = new IntersectionObserver(loadMore, { threshold: 1.0 });
+		observer.observe(loadMoreElement);
 	});
 
 	onDestroy(() => {
-		if (element) {
-			element.removeEventListener('scroll', onScroll);
-			element.removeEventListener('resize', onScroll);
+		if (observer) {
+			observer.disconnect();
 		}
 	});
 </script>
 
-{#if !window && !elementScroll}
-	<div bind:this={component} id="svelte-infinite-scroll" style="width: 0;" />
-{/if}
+<div bind:this={loadMoreElement} id="svelte-infinite-scroll" style="width: 0;" />
